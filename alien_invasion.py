5841c47edf59c52e4a551997b374f7b87a2fa195
nefulan <2426398674@qq.com>
2020-12-05 16:55:09 +0800

    第一次提交

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import sys
+
+import pygame
+from setting import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    pygame.init()
+    ai_setting = Settings()
+    screen = pygame.display.set_mode((ai_setting.screen_width,
+                                      ai_setting.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    # 创建飞船
+    ship = Ship(ai_setting, screen)
+
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_setting, screen, ship)
+
+
+run_game()
